afaf6c14d4f31baea847933fe25c3c404caa2f5d
rpugh-lgtm <rpugh@shelbyed.org>
2025-08-28 11:56:24 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,30 @@
-// A simple service worker to enable PWA features
-const CACHE_NAME = 'slides-kiosk-v1';
+const CACHE_NAME = 'kiosk-cache-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
+    '/',
+    '/SCS_Kiosks.html',
+    '/manifest.json',
+    '/images/icon-192x192.png',
+    '/images/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
-    // Perform installation steps
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then((response) => {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
